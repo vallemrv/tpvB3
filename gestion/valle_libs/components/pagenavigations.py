a6ea275c552a8d676f6615d1e43ca8ac9ebaf24d
Manuel Rodriguez <valle.mrv@gmail.com>
2017-07-20 03:42:55 +0200

    initial commit

--- gestion/valle_libs/components/pagenavigations.py
+++ gestion/valle_libs/components/pagenavigations.py
@@ -3,7 +3,7 @@
 # @Email:  valle.mrv@gmail.com
 # @Filename: pagenavigations.py
 # @Last modified by:   valle
-# @Last modified time: 15-Jul-2017
+# @Last modified time: 19-Jul-2017
 # @License: Apache license vesion 2.0
 
 from kivy.uix.relativelayout import RelativeLayout
@@ -12,99 +12,12 @@ from kivy.properties import (StringProperty, ListProperty,
                              ObjectProperty, DictProperty)
 from kivy.animation import Animation
 from kivy.lang import Builder
+import components.resources as res
 
-Builder.load_string('''
-#:import LabelColor components.labels.LabelColor
-#:import ButtonIcon components.buttons.ButtonIcon
-#:import get_color kivy.utils.get_color_from_hex
-
-<MainPage>:
-    content_page: _content_page
-    size_hint: 1, 1
-    title_bgColor: '#ffffff'
-    title: ''
-    BoxLayout:
-        orientation: 'vertical'
-        size_hint: 1, 1
-        AnchorLayout:
-            id: _header
-            size_hint: 1, None
-            height: dp(50)
-            anchor_y: 'top'
-            anchor_x: 'center'
-            canvas:
-                Color:
-                    rgb: 0,0,0
-                Rectangle:
-                    size: self.size
-                    pos: self.pos
-            LabelColor:
-                size_hint: 1, .99
-                text: root.title
-                bgColor: root.title_bgColor
-                border_size: 0
-                font_size: '20dp'
-
-
-        AnchorLayout:
-            id: _content_page
-            size_hint: 1, 1
-
-<Page>:
-    content_page: _content_page
-    size_hint: 1, 1
-    title_bgColor: '#ffffff'
-    title: ''
-    BoxLayout:
-        orientation: 'vertical'
-        size_hint: 1, 1
-        AnchorLayout:
-            id: _header
-            size_hint: 1, None
-            height: dp(50)
-            anchor_y: 'top'
-            anchor_x: 'center'
-            canvas:
-                Color:
-                    rgb: 0,0,0
-                Rectangle:
-                    size: self.size
-                    pos: self.pos
-            LabelColor:
-                size_hint: 1, .99
-                text: root.title
-                bgColor: root.title_bgColor
-                border_size: 0
-                font_size: '20dp'
-            AnchorLayout:
-                anchor_y: 'center'
-                anchor_x: 'right'
-                size_hint: .85, 1
-                ButtonIcon:
-                    size_hint: None, .7
-                    width: dp(60)
-                    text: 'back'
-                    icon: res.FA_ANGLE_LEFT
-                    orientation: 'horizontal'
-                    font_size: '15dp'
-                    border_size: 0
-                    bgColor:'#ecd5c4'
-                    on_release: root.parent.back_page()
-
-        AnchorLayout:
-            id: _content_page
-            size_hint: 1, 1
-            canvas.before:
-                Color:
-                    rgb: get_color(root.bgColor)
-                Rectangle:
-                    size: self.size
-                    pos: self.pos
-
-''')
+Builder.load_file(res.get_kv("pagenavigations"))
 
 class MainPage(RelativeLayout):
-    title = StringProperty()
+    title = StringProperty('')
     title_bgColor = StringProperty("#ffffff")
     page_manager = ObjectProperty(None)
 
@@ -120,7 +33,7 @@ class MainPage(RelativeLayout):
 
 
 class Page(RelativeLayout):
-    title = StringProperty()
+    title = StringProperty('')
     title_bgColor = StringProperty("#ffffff")
     id_page = StringProperty("")
     bgColor = StringProperty("#ffffff")
@@ -132,6 +45,13 @@ class Page(RelativeLayout):
         else:
             self.content_page.add_widget(widget)
 
+    def collide_point(self, x, y):
+       return (x > self.x and x < self.x +self.width) and (y > self.y and y < self.y +self.height)
+
+    def on_touch_down(self, touch, *args):
+       super(Page, self).on_touch_down(touch)
+       if self.collide_point(touch.x, touch.y):
+           return True
 
 
 class PageManager(FloatLayout):
@@ -142,34 +62,29 @@ class PageManager(FloatLayout):
         super(PageManager, self).__init__(**kargs)
 
     def add_widget(self, widget):
-        try:
-            widget.page_manager = self
-        except:
-            raise ("Error", "Widget not is MainPage or Page types")
-
+        widget.page_manager = self
         if type(widget) is MainPage:
             self.stack_pages.append(widget)
-            super(PageManager,self).add_widget(widget)
         elif type(widget) is Page:
             widget.bind(id_page=self.on_id_pages)
+
+        super(PageManager,self).add_widget(widget)
+
+    def on_width(self, w, val):
+        for child in self.pages.values():
+            child.pos = val +10, 0
 
     def on_id_pages(self, w, val):
         self.pages[val] = w
 
+
     def navigate(self, nav):
         w = self.pages[nav]
-        w.pos = self.width+10, 0
         self.stack_pages.append(self.pages[nav])
-        super(PageManager, self).add_widget(w)
         ai = Animation(x=0, duration=.1)
         ai.start(w)
 
     def back_page(self):
-        w = self.stack_pages[-1]
-        ai = Animation(x=self.width+10, duration=.1)
-        ai.bind(on_complete=self.on_complete)
-        ai.start(w)
-
-    def on_complete(self, ani, w):
         w = self.stack_pages.pop()
-        self.remove_widget(w)
+        ai = Animation(x=self.width+10, duration=.1)
+        ai.start(w)
